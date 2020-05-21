f85e255c76713a46b68507e865ca6c743975708b
liuxiaobing <liuxiaobing@sensetime.com>
2020-05-21 17:40:13 +0800

    Initial Commit

---
+++ SwiftUiKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftUiKit
+//
+//  Created by Liu Xiaobing on 2020/5/21.
+//  Copyright © 2020 Liu HuLai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
